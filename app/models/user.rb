a0a0f462b3ccd15f75eb7f7200e6df04cd2013c2
1 <q1@q1.(none)>
2014-03-30 23:08:04 +0300

    add error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# encoding: utf-8
 class User < ActiveRecord::Base
-	has_secure_password
+has_secure_password 
+
+
+	validates_presence_of :name, :email, message: "не должен быть пустой"
+	validates :email, :format => {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "не верный формат" }, :uniqueness => true
+	
+validates :name, :uniqueness => true, :format => {with: /[a-zA-ZА-Яa-я_1-90]/ , message: "недоступное" }
+
+  validates_length_of :password, :within => 5..30,
+    too_long: "слишком длинный",
+    too_short: "должен состоять из 5 символов и более"
+
 end
